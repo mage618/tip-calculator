5763bad82791bfeedb30d0e25b6e0b9d0d48332d
hyy <yh8953@utexas.edu>
2023-04-24 17:28:56 -0500

    add app bar

--- app/src/main/java/com/heyi/tippy/MainActivity.kt
+++ app/src/main/java/com/heyi/tippy/MainActivity.kt
@@ -26,6 +26,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.my_toolbar))
+
         etBaseAmount = findViewById(R.id.etBaseAmount)
         seekBarTip = findViewById(R.id.seekBarTip)
         tvTipPercentLabel = findViewById(R.id.tvTipPercentLabel)
